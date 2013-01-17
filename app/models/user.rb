e732a1b038ca669875cd6c593c72217e6a794a00
matthewg <matthew.green@datacom.co.nz>
2013-01-18 09:38:22 +1300

    user model now has secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :phone_number
+  attr_accessible :email, :name, :password, :password_confirmation, :phone_number
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
