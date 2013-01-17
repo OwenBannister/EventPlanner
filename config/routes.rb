ac124196c7c964455a37e26347b1d4d654b2cacb
matthewg <matthew.green@datacom.co.nz>
2013-01-18 10:09:26 +1300

    user signup now functional

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ EventPlanner::Application.routes.draw do
 
   root :to => "events#index"
 
+  match '/signup', to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
