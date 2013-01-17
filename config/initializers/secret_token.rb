636a85747f929f625567ba0ee945f480e880f62d
OwenB <Owen.Bannister@datacom.co.nz>
2013-01-17 16:21:30 +1300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventPlanner::Application.config.secret_token = '5ac19162dca224e71d43efada344efddb6584ec5814595b6470af99635c63ecb1e38496880c8a730641164961eb05b1eda9083ca48c0c663557930f82bd62ccd'
