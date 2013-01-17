4ca6bb10238f299638fad477494e13f812f4fb49
OwenB <Owen.Bannister@datacom.co.nz>
2013-01-17 21:39:15 +1300

    view other users on same event

--- db/schema.rb
+++ db/schema.rb
@@ -39,9 +39,6 @@ ActiveRecord::Schema.define(:version => 20130117230025) do
     t.string   "email"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-    t.string   "remember_token"
   end
-
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
