94361373ca2a8d32f27b26fbba31aca29d750e99
matthewg <matthew.green@datacom.co.nz>
2013-01-18 14:34:32 +1300

    removed 20130117213222_rename_column.rb

--- db/migrate/20130117213222_rename_column.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumn < ActiveRecord::Migration
-  def up
-  	rename_column :enrollments, :student_id, :user_id
-  end
-
-  def down
-  	rename_column :enrollments, :user_id, :student_id 
-  end
-end
